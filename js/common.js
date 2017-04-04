5049f8e5106ecb4d27a4a1d9889aa9dc43356c06
3axap4eHko <3axap4eHko@gmail.com>
2017-04-03 20:06:07 -0400

    version 0.0.60

---
+++ js/common.js
@@ -0,0 +1,20 @@
+
+function loadScript(url, attributes) {
+  const d = document, s = d.createElement('script');
+  s.src = url;
+  s.async = true;
+  Object.keys(attributes).forEach( name => s.setAttribute(name, attributes[name]));
+  d.body.appendChild(s);
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
--- js/theme.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function loadScript(url, attributes) {
-    const d = document, s = d.createElement('script');
-    s.src = url;
-    s.async = true;
-    Object.keys(attributes).forEach( name => s.setAttribute(name, attributes[name]));
-    d.body.appendChild(s);
-}
