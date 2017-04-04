aee3ff755dc478e5e897e7fa4f86a8358cc0f2c8
3axap4eHko <3axap4eHko@gmail.com>
2017-04-03 20:10:30 -0400

    version 0.0.62

--- js/common.js
+++ js/common.js
@@ -9,7 +9,7 @@ function loadScript(url, attributes) {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function(err) {
